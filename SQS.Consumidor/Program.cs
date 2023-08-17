5753e23997770c19bdcbc5042a3ee4331f9b49db
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-08-16 22:21:44 -0300

    ...

---
+++ SQS.Consumidor/Program.cs
@@ -0,0 +1,40 @@
+﻿using Amazon.SQS.Model;
+using Amazon.SQS;
+using Amazon;
+using Amazon.Runtime.Internal.Endpoints.StandardLibrary;
+using DotNetEnv;
+
+internal class Program
+{
+    static string awsEndpoint = Environment.GetEnvironmentVariable("AWSENDPOINT") ?? string.Empty;
+    private static async Task Main(string[] args)
+    {
+        
+        DotNetEnv.Env.Load();
+
+        var client = new AmazonSQSClient(RegionEndpoint.USWest2);
+
+        var request = new ReceiveMessageRequest
+        {
+            QueueUrl = awsEndpoint
+        };
+
+        // EX: As long as there are messages in the queue the client will be called
+        while (true)
+        {
+            var response = await client.ReceiveMessageAsync(request);
+
+            // EX: Doing for each to access queue consumption response
+            foreach (var message in response.Messages)
+            {
+                // EX: While messages such as catapimbas or carambola are in the file, they will be deleted
+                List<string> list = new List<string> { "catapimbas", "carambolas" };
+                while (list.Contains(message.Body))
+                {
+                    Console.WriteLine(message.Body);
+                    await client.DeleteMessageAsync(awsEndpoint, message.ReceiptHandle);
+                }
+            }
+        }
+    }
+}
---
+++ SQS.Fornecedor/Program.cs
@@ -0,0 +1,28 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Threading.Tasks;
+using DotNetEnv;
+
+internal class Program
+{
+
+    static string awsEndpoint = Environment.GetEnvironmentVariable("AWSENDPOINT") ?? string.Empty;
+    private static async Task Main(string[] args)
+    {
+        DotNetEnv.Env.Load();
+
+        var client = new AmazonSQSClient(RegionEndpoint.USWest2);
+
+        var request = new SendMessageRequest
+        {
+            QueueUrl = awsEndpoint,
+            MessageBody = "peixe"
+        };
+
+        while (true)
+        {
+            var response = await client.SendMessageAsync(request);
+        }
+    }
+}
